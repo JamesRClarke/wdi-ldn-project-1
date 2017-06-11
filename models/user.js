042ebd0093fbed07ed3663e605686fd0ed25d736
James Clarke <jamesraymondclarke@gmail.com>
2017-06-11 21:57:17 +0100

    delete button not working for landmarks/show.ejs page

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
   firstName: { type: String, minlength: 2, required: true, trim: true },
   lastName: { type: String, minlength: 2, required: true, trim: true },
+  gender: {type: String, enum: ['Male', 'Female', 'Other']},
   email: { type: String, required: true, trim: true, unique: true },
   username: { type: String, required: true, trim: true, unique: true },
   password: { type: String, required: true }
